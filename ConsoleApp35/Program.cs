f82e7a5f2d3ee90bf6de1231b5c306a377e1bc6c
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-03-10 08:30:26 +0100

    Add project files.

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Inluppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välj ett av följande alternativ:");
+            Console.WriteLine("1. Subtrahera ett tal med ett annat");
+            Console.WriteLine("2. Dividera ett tal med ett annat");
+            Console.WriteLine("3. Avsluta programmet"); 
+
+            string val = Console.ReadLine();
+
+            switch (val)
+            {
+                case "1":
+                {
+                    Console.Write("Välj ett tal: ");
+                    int tal1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Välj ett till tal: ");
+                    int tal2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(tal1 + " - " + tal2 + " = " + (tal1 - tal2));
+
+                    break;
+                }
+
+                case "2":
+                {
+                    Console.Write("Välj ett tal: ");
+                    int tal1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Välj ett till tal: ");
+                    int tal2 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(tal1 + " / " + tal2 + " = " + (tal1 / tal2));
+
+                    break;
+                }
+                case "3":
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
